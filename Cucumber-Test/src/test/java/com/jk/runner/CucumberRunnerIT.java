52bfa70aaa3ee21b78b7f0a1d2c1b645ed680057
Junaid-Khan <80524662+JunaidKhan44@users.noreply.github.com>
2024-06-20 15:30:48 +0500

    Update CucumberRunnerIT.java

--- Cucumber-Test/src/test/java/com/jk/runner/CucumberRunnerIT.java
+++ Cucumber-Test/src/test/java/com/jk/runner/CucumberRunnerIT.java
@@ -3,6 +3,10 @@ package com.jk.runner;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
+/**
+ * @author Junaid.Khan
+ */
+
 @CucumberOptions(
         features = "src/test/resources/test.feature",
         glue = {"com.jk.test"}
